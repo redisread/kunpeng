9eb5ef0197570dfc6e8b1713bb90bae23a8dd8e0
victorhongdream <1427298682@qq.com>
2020-03-21 15:48:57 +0800

    gf

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'tmvf0bx$$n75fujjcsur4&8l%-b6id4dmt)l6mwy(pf-n%d4hk'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
